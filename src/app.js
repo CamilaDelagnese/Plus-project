590755923c3bfa73ba3111502a06f361336d299f
cex <cex@0s-MacBook-Air.local>
2021-02-11 19:29:05 +0000

    add unit conversion

--- src/app.js
+++ src/app.js
@@ -29,6 +29,8 @@ function formatDate (timestamp){
     let dateElement = document.querySelector("#date");
     let iconElement = document.querySelector("#icon");
 
+    celsiusTemperature = response.data.main.temp;
+
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
@@ -49,10 +51,35 @@ axios.get(apiUrl).then(displayTemperature);
 }
 
 function handleSubmit(event){ 
-event.preventDefaut();
 let cityInputElement = document.querySelector("#city-input");
 search(cityInputElement.value);
 }
-search("london");
+
+function displayFahrenheitTemperature(event){
+let temperatureElement = document.querySelector("#temperature");
+
+celsiusLink.classList.remove("active");
+fahrenheitLink.classList.add("active");
+let fahrenheitTemperature = (celsiusTemperature *9) /5+32;
+temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function displayCelsiusTemperature(event){
+celsiusLink.classList.add("active");
+fahrenheitLink.classList.remove("active");
+let temperatureElement = document.querySelector("#temperature");
+temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+let celsiusTemperature = null;
+
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+search("london");
