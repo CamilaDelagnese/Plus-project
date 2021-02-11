9633568678df62324e0b22b3d19160a288e53e21
CamilaDelagnese <77103663+CamilaDelagnese@users.noreply.github.com>
2021-02-11 09:44:35 +0000

    add some update

--- src/app.js
+++ src/app.js
@@ -2,12 +2,21 @@ function formatDate (timestamp){
     let date = new date(timestamp);
 
     let hours = date.getHours();
-    if (hours < 10){hours = `0${hours}`;}
+    if (hours < 10){hours = `0${hours}`;
+}
 
     let minutes = date.getMinutes();
-    if (minutes < 10){minutes = `0${minutes}`;}
+    if (minutes < 10){minutes = `0${minutes}`;
+}
 
-    let days = [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday","Saturday" ];
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"];
     let day = days[date.getDay()];
     return `${day} ${hours}:${minutes}`;
 }
@@ -15,18 +24,25 @@ function formatDate (timestamp){
     let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
     let descriptionElement = document.querySelector("#description");
-    let humidityElement = document.querySelector("humidity");
+    let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
+    let dateElement = document.querySelector("#date");
+    let iconElement = document.querySelector("#icon");
 
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
+    dateElement.innerHTML = formatDate(response.date.dt * 1000);
+    iconElement.setAttribute(
+        "src",
+        `http://openweather.org/img/wn/04d02x.png`);
+        iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "638814fa6995b0d81c07ae759b949580";
-let city = "Paris"
+let city = "London"
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 
